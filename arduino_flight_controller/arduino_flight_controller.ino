5146ed22b9a573116e32f12110fca35a159a35db
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2017-09-28 11:01:38 -0300

    Removing old comments

--- arduino_flight_controller/arduino_flight_controller.ino
+++ arduino_flight_controller/arduino_flight_controller.ino
@@ -287,25 +287,6 @@ void setMotor() {
   motorB.writeMicroseconds(normalize(cmd_motB));
   motorC.writeMicroseconds(normalize(cmd_motC));
   motorD.writeMicroseconds(normalize(cmd_motD));
-
-  // if (value < MIN_SIGNAL) {
-  //   value = MIN_SIGNAL;
-  // } else if (value > MAX_SIGNAL) {
-  //   value = MAX_SIGNAL;
-  // }
-
-  // // Serial.print("Set motor 1: ");
-  // // logger.logStatus(value);
-  // motor1.writeMicroseconds(value);
-  // // Serial.print("Set motor 2: ");
-  // // logger.logStatus(value);
-  // motor2.writeMicroseconds(value);
-  // // Serial.print("Set motor 3: ");
-  // // logger.logStatus(value);
-  // motor3.writeMicroseconds(value);
-  // // Serial.print("Set motor 4: ");
-  // // logger.logStatus(value);
-  // motor4.writeMicroseconds(value);
 }
 
 void initSensors() {
@@ -328,45 +309,6 @@ void initSensors() {
   // Set threshold sensivty. Default 3.
   // If you don't want use threshold, comment this line or set 0.
   mpu.setThreshold(3);
-
-  // Wire.begin();
-  // Wire.beginTransmission(MPU);
-  // Wire.write(0x6B);
-  // Wire.write(0); 
-  // Wire.endTransmission(true);
-  // delay(100);
-
-  // //setup starting angle
-  // //1) collect the data
-  // Wire.beginTransmission(MPU);
-  // Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  // Wire.endTransmission(false);
-  // Wire.requestFrom(MPU,6,true);  // request a total of 14 registers
-  // AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  // AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  // AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  // // Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  
-  // Wire.beginTransmission(MPU);
-  // Wire.write(0x43);  // starting with register 0x3B (ACCEL_XOUT_H)
-  // Wire.endTransmission(false);
-  // Wire.requestFrom(MPU,6,true);  // request a total of 14 registers
-  // GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  // GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  // GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  // //2) calculate pitch and roll
-  // double roll = atan2(AcY, AcZ)*RAD_TO_DEG;
-  // double pitch = atan2(-AcX, AcZ)*RAD_TO_DEG;
-
-  // //3) set the starting angle to this pitch and roll
-  // // double gyroXangle = roll;
-  // // double gyroYangle = pitch;
-  // compAngleX = roll;
-  // compAngleY = pitch;
-
-  // //start a timer
-  // timer = micros();
 }
 
 void readSensors() {
@@ -382,60 +324,10 @@ void readSensors() {
   rollAngle = rollAngle + norm.XAxis * timeStep;
   yawAngle = yawAngle + norm.ZAxis * timeStep;
 
-  // Wire.beginTransmission(MPU);
-  // Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  // Wire.endTransmission(false);
-  // Wire.requestFrom(MPU,6,true);  // request a total of 14 registers
-  // AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  // AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  // AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  // // Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  
-  // Wire.beginTransmission(MPU);
-  // Wire.write(0x43);  // starting with register 0x3B (ACCEL_XOUT_H)
-  // Wire.endTransmission(false);
-  // Wire.requestFrom(MPU,6,true);  // request a total of 14 registers
-  // GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  // GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  // GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-
-  // double dt = (double)(timer - timerPrev) / 1000000; //This line does three things: 1) stops the timer, 2)converts the timer's output to seconds from microseconds, 3)casts the value as a double saved to "dt".
-
-  // //the next two lines calculate the orientation of the accelerometer relative to the earth and convert the output of atan2 from radians to degrees
-  // //We will use this data to correct any cumulative errors in the orientation that the gyroscope develops.
-  // double roll = atan2(AcY, AcZ)*RAD_TO_DEG;
-  // double pitch = atan2(-AcX, AcZ)*RAD_TO_DEG;
-
-  // //The gyroscope outputs angular velocities.  To convert these velocities from the raw data to deg/second, divide by 131.  
-  // //Notice, we're dividing by a double "131.0" instead of the int 131.
-  // double gyroXrate = GyX/131.0;
-  // double gyroYrate = GyY/131.0;
-
-  // //THE COMPLEMENTARY FILTER
-  // //This filter calculates the angle based MOSTLY on integrating the angular velocity to an angular displacement.
-  // //dt, recall, is the time between gathering data from the MPU6050.  We'll pretend that the 
-  // //angular velocity has remained constant over the time dt, and multiply angular velocity by 
-  // //time to get displacement.
-  // //The filter then adds a small correcting factor from the accelerometer ("roll" or "pitch"), so the gyroscope knows which way is down. 
-  // compAngleX = 0.99 * (compAngleX + gyroXrate * dt) + 0.01 * roll; // Calculate the angle using a Complimentary filter
-  // compAngleY = 0.99 * (compAngleY + gyroYrate * dt) + 0.01 * pitch; 
-
   logger.logVariable("yawAngle", yawAngle);
   logger.logVariable("pitchAngle", pitchAngle);
   logger.logVariable("rollAngle", rollAngle);
-  
-  /*Now we have our angles in degree and values from -10º0 to 100º aprox*/
-
-  //Envia valor da temperatura para a serial e o LCD
-  //Calcula a temperatura em graus Celsius
-  // Serial.print("Tmp="); Serial.print(Tmp/340.00+36.53);
 }
-
-// A special thanks to Electronoobs
-// https://www.youtube.com/watch?v=if9LZTcy_uk&index=1&list=LL70ziqu5-D8VBDmxa5HUYwg
-//This is the interruption routine
-//----------------------------------------------
 
 ISR(PCINT0_vect){
   //First we take the current count value in micro seconds using the micros() function
